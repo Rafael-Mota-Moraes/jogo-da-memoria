7dd29e9fab70612ae1ff9215db309594b52f7bbb
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-04-19 14:54:48 -0300

    first commit

---
+++ src/components/GridItem/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+type ContainerProps = {
+    showBackground: boolean;
+};
+
+type IconProps = {
+    opacity?: number;
+};
+
+export const Container = styled.div<ContainerProps>`
+    height: 100px;
+
+    background-color: ${(props) =>
+        props.showBackground ? "#1550ff" : "#e2e3e3"};
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+`;
+
+export const Icon = styled.img<IconProps>`
+    width: 40px;
+    height: 40px;
+
+    opacity: ${(props) => (props.opacity ? props.opacity : 1)};
+`;
